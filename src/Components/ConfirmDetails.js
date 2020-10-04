8f2a9b549c473cb9f82c5b4d3a2e3dc05c729749
Ashish Sharma <ashish.shm09@gmail.com>
2020-10-04 07:22:12 +0530

    Added Confirm Page

--- src/Components/ConfirmDetails.js
+++ src/Components/ConfirmDetails.js
@@ -1,40 +1,128 @@
 import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
 
 export class ConfirmDetails extends Component {
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  goBack = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
   render() {
     let { values } = this.props;
-    let newvalues = Object.keys(values);
-    newvalues.shift();
 
     return (
       <>
-        <div>
-          {newvalues.map((el) => {
-            return (
-              <div className="flex">
-                <p>{el}</p>
-                <p>{values.el}</p>
-              </div>
-            );
-          })}
-        </div>
-
-        <div className="flex">
+        <div className="inputfield">
           <div>
-            <button className="nextbtn" onClick={this.goBack}>
-              Back
-            </button>
+            <TextField
+              style={styles.input}
+              label="Username"
+              defaultValue={values.username}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="Bio"
+              defaultValue={values.bio}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="E-mail"
+              defaultValue={values.email}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="Contact Number"
+              defaultValue={values.phone}
+              variant="outlined"
+            />
           </div>
 
           <div>
-            <button className="nextbtn" onClick={this.continue}>
-              Next
-            </button>
+            <TextField
+              style={styles.input}
+              label="First Name"
+              defaultValue={values.fname}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="Last Name"
+              defaultValue={values.lname}
+              variant="outlined"
+            />
+          </div>
+
+          <div>
+            <TextField
+              style={styles.input}
+              label="Address"
+              defaultValue={values.address}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="City"
+              defaultValue={values.city}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="Pincode"
+              defaultValue={values.pin}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              style={styles.input}
+              label="Country"
+              defaultValue={values.country}
+              variant="outlined"
+            />
+          </div>
+
+          <div className="flex">
+            <div>
+              <button className="nextbtn" onClick={this.goBack}>
+                Back
+              </button>
+            </div>
+
+            <div>
+              <button className="nextbtn" onClick={this.continue}>
+                Confirm and Continue
+              </button>
+            </div>
           </div>
         </div>
       </>
     );
   }
 }
+
+const styles = {
+  input: {
+    marginTop: 40,
+  },
+};
 
 export default ConfirmDetails;
